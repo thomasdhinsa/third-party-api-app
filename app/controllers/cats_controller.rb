81f0ed76b80c99f32c72fff38dc3d9596441f8c3
Tom Dhinsa <mrdhinsa05@gmail.com>
2022-02-02 11:38:29 -0500

    adds working third party api access

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -2,7 +2,7 @@ class CatsController < ApplicationController
 
 
   def index
-    # cats = HTTP.get("https://api.thecatapi.com/v1/breeds").parse(:json)
+    cats = HTTP.auth("your API key").get("https://api.thecatapi.com/v1/breeds").parse(:json)
     render json: cats
   end
   
